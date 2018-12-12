1b560a661c7e3e48270f81656ca5eef01bf36173
Francesco d'Aloisio <fdaloisio@gmail.com>
2018-12-12 17:17:19 +0100

    changed PRG_HOME

--- start-it.sh
+++ start-it.sh
@@ -8,8 +8,8 @@ OUTFILE=
 PIDFILE="/tmp/$NAME.pid"
 
 BASENAME=`basename $0`
-PRG_HOME=`echo $0| sed "s/$BASENAME//g"`
-cd $PRG_HOME
+PRG_HOME="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+cd "$PRG_HOME"
 
 pid=`cat $PIDFILE`
 PIDEXIST=$([ -n $pid -a -d "/proc/$pid" ] && echo $pid || echo "")
